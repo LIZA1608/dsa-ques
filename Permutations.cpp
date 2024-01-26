e5585e316193514b58ce7a9aac77dd2bebb0b63f
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2024-01-26 17:37:11 +0530

    Create Permutations.cpp

---
+++ Permutations.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+void solve(vector<vector<int>>&ans,vector<int>vec,int freq[],vector<int>temp){
+    if(temp.size()==vec.size()){
+        ans.push_back(temp);
+        return;
+    }
+    for(int i=0;i<n;i++){
+        if(!freq[i]){
+            temp.push_back(vec[i]);
+            freq[i]=1;
+           solve(ans,vec,freq,temp);
+           freq[i]=0;
+           temp.pop_back();
+        }
+    }
+}
+vector<vector<int>> permutations(vector<int> &vec, int n) {
+    // Write your code here.
+    vector<vector<int>>ans;
+    vector<int>temp;
+    int freq[n];
+    for(int i=0;i<n;i++){
+        freq[i]=0;
+    }
+    solve(ans,vec,freq,temp);
+    return ans;
+
+}
